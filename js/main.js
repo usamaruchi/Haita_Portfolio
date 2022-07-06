bc0123fee20f9c7b89e225a7205e7bcead8131c9
Han <usamaruchi@gmail.com>
2022-07-06 11:44:33 +0800

    0706

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// .nextpage_btn 按鈕
+$(function () {
+    setTimeout(function () {
+        $(".nextpage_btn").show();
+    }, 5000);
+
+    setTimeout(function () {
+        $("#load_video").hide();
+    }, 8000);
+
+})
+
