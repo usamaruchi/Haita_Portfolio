432e76f0610c1236deafa159ff49ce1c1fc859e7
haita <usamaruchi@gmail.com>
2022-07-08 14:35:02 +0800

    0708

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Load
 // .nextpage_btn 按鈕
 $(function () {
     setTimeout(function () {
@@ -8,5 +9,10 @@ $(function () {
         $("#load_video").hide();
     }, 8000);
 
+})
+
+// Navigation bar
+window.addEventListener('scroll', function(){
+    console.log('Hi!');
 })
 
