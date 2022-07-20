aec37a2eeb964373d7a974118497f1ab87e73af5
Han <usamaruchi@gmail.com>
2022-07-20 15:50:59 +0800

    0720

--- js/main.js
+++ js/main.js
@@ -11,6 +11,59 @@ $(function () {
 
 })
 
+// Scroll to top
+const scrollTopBtn = document.querySelector(".scrollToTop_btn");
+
+window.addEventListener("scroll", function () {
+    scrollTopBtn.classList.toggle("active", window.scrollY > 500);
+})
+
+scrollTopBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+window.addEventListener("scroll", () => {
+    const sections = document.querySelectorAll("section");
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight;
+        let sectionTop = current.offsetTop - 50;
+        let id = current.getAttribute("id");
+
+        console.log("sectionTop = ", sectionTop);
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector(".nav_items a[href*=" + id + "]").classList.add("active");
+        } else {
+            document.querySelector(".nav_items a[href*=" + id + "]").classList.remove("active");
+        }
+    });
+});
+
+//  Light/Dark
+const themeBtn = document.querySelector(".theme_btn");
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark_theme");
+    themeBtn.classList.toggle("sun");
+
+    localStorage.setItem("saved_theme", getCurrentTheme());
+    localStorage.setItem("save_icon", getCurrentIcon());
+})
+
+const getCurrentTheme = () => document.body.classList.contains("dark_theme") ? "dark" : "light";
+
+const getCurrentIcon = () => themeBtn.classList.contains("sun") ? "sun" : "moon";
+
+const savedTheme = localStorage.getItem("saved_theme");
+const savedIcon = localStorage.getItem("saved_icon");
+
+if (savedTheme) {
+    document.body.classList[savedTheme === "dark" ? "add" : "remove"]("dark_theme");
+    themeBtn.classList[savedIcon === "sun" ? "add" : "remove"]("sun");
+}
+
 // Navigation bar
 window.addEventListener('scroll', function () {
     // console.log('Hi!');
@@ -52,6 +105,7 @@ portfolioCloseBtns.forEach((portfolioCloseBtn) => {
         portfolioModals.forEach((portfolioModalView) => {
             portfolioModalView.classList.remove("active");
         });
-    })
+    });
 })
+
 
